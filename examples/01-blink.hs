95b3af5c00e24f4d1e360a4d6231668aa750c96c
BassGT <springtrap9397@gmail.com>
2026-08-03 01:00:13 +0000

    feat: 2 examples were add

--- examples/01-blink.hs
+++ examples/01-blink.hs
@@ -1,12 +1,45 @@
--- To turn on a simple you should follow the next steps
-{- First, import the function withChip in 'Fuyu.GPIO.Chip'
-   (alternative we could use the umbrella module Fuyu.GPIO)
-   
--} 
+-- To blink LED 10 times we could follow the next steps
 module Main where
-import Fuyu.GPIO.Chip (withChip)
-import qualified Fuyu.GPIO.Line as Line  
 
+import Fuyu.GPIO.Chip (withChip, isGPIOChip)
+import Fuyu.GPIO.Line (withSettings, withConfig, withRequest)
+import qualified Fuyu.GPIO.Line as Line
 
-main :: IO () 
-main = undefined
+import Control.Monad (replicateM_)
+import Data.Vector.Storable (singleton)
+import Control.Concurrent (threadDelay)
+
+-- We could check if this file is a GPIO Chip with 'isGPIOChip' function
+chipPath :: FilePath
+chipPath = "/dev/gpiochip0" 
+
+-- In Orange Pi devices we could find this information with 'gpio readall' command (or in docs)
+-- In this case the line offset 269 corresponds to physical pin 7 
+ledOffset :: Line.Offset 
+ledOffset = Line.offset 269 
+
+main :: IO ()
+main = do
+   isChip <- isGPIOChip chipPath
+   if isChip
+     then do 
+       putStrLn "Example started: LED blinking"
+       runApp
+     else putStrLn $ chipPath ++ " does not correspond to a valid GPIO Chip"
+
+runApp :: IO ()
+runApp = do
+   withChip chipPath $ \chip -> do
+      withSettings $ \settings -> do
+         Line.setDirection settings Line.DirOutput
+         withConfig $ \config -> do
+            Line.addSettings config (singleton ledOffset) settings
+            -- The use of Nothing instead of a 'RequestConfig' means that we are using a NULL request configuration object.
+            -- (Do not confuse Fuyu.GPIO.RequestConfig with Fuyu.GPIO.Line.Config, the first one is used for kernel options
+            -- and the second one is used for line config).  
+            withRequest chip Nothing config $ \request -> do
+               replicateM_ 10 $ do 
+                 Line.setValue request ledOffset Line.Active
+                 threadDelay 500000 -- 0.5 seconds pause   
+                 Line.setValue request ledOffset Line.Inactive
+                 threadDelay 500000 -- 0.5 seconds pause
---
+++ examples/02-button.hs
@@ -0,0 +1,64 @@
+-- In this example we will learn how to handle a button input using libgpiod line settings.
+module Main where
+import Fuyu.GPIO.Chip (withChip)
+import Fuyu.GPIO.Line as Line 
+import Fuyu.GPIO.EdgeEvent as Event
+
+import Data.Vector.Storable (singleton)
+import Control.Monad (forever)
+import Control.Exception (catch, IOException)
+
+chipPath :: FilePath
+chipPath = "/dev/gpiochip0" 
+
+-- This constant defines the maximum duration 'waitEdgeEvents' will wait for an event.
+-- (This timeout can also be configured as infinite or immediate).
+fiveSecondsNs :: Timeout
+fiveSecondsNs = Nanoseconds 5000000000 
+
+-- Do not confuse this with kernel ring buffer capacity. 'Capacity' refers to the user-space event buffer.
+-- It is clamped between 1 and 1024, and must be constructed via 'userBufferCapacity'
+-- (passing 0 defaults to 64).
+bufferCapacity :: Capacity
+bufferCapacity = userBufferCapacity 1 
+ 
+buttonOffset :: Offset
+buttonOffset = offset 257 
+
+
+buttonSettings :: Settings -> IO ()
+buttonSettings stgs = do
+  Line.setDirection stgs DirInput     -- Configure line as input mode
+  Line.setBias stgs BiasPullUp        -- Enable internal pull-up resistor
+                                      -- (the physical button connects GND when pressed, driving the line to Inactive)
+  Line.setDebouncePeriodUs stgs 20000 -- 20ms debounce period to filter out mechanical contact bounce without threadDelay
+  Line.setEdgeDetection stgs EdgeBoth -- Listen for both Rising and Falling edge transitions
+
+buttonWorker :: Request -> Buffer -> IO ()
+buttonWorker req buf = do
+  res <- Event.waitEdgeEvents req fiveSecondsNs
+  case res of 
+    EventReady readyReq -> do
+      events <- Event.readEdgeEvents readyReq buf -- Read events from user buffer (configured with capacity 1)
+      print events    
+    TimeoutResult -> putStrLn "Timeout: No event was read" -- Printed after the 5-second wait timeout expires
+  
+main :: IO ()
+main = do
+  -- Catch exception to exit cleanly on Ctrl+C
+  runApp `catch` handleExit 
+  where
+    handleExit :: IOException -> IO ()
+    handleExit _ = putStrLn "\nLoop terminated successfully!"
+
+runApp :: IO ()
+runApp = do
+  withChip chipPath $ \chip -> do
+    withSettings $ \settings -> do
+      buttonSettings settings
+      withConfig $ \config -> do
+        Line.addSettings config (singleton buttonOffset) settings
+        withRequest chip Nothing config $ \request -> do
+          withEventBuffer bufferCapacity $ \buffer -> do
+            putStrLn "Loop started: Press the button to generate events or Ctrl+C to exit"
+            forever (buttonWorker request buffer)
